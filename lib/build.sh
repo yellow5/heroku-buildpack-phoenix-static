8609e4e466f09b237857fd2ffcb7903ad97e9f6e
Cameron Dykes <yellow5c@gmail.com>
2025-06-18 12:05:30 -0500

    Update build.sh
    Debug entries to see what is happening in Heroku

--- lib/build.sh
+++ lib/build.sh
@@ -55,6 +55,8 @@ download_node() {
     resolve_node
 
     echo "Downloading and installing node $number..."
+    echo "DEBUG - url: $url"
+    echo "DEBUG - cached_node: $cached_node"
     local code=$(curl "$url" -L --silent --fail --retry 5 --retry-max-time 15 -o ${cached_node} --write-out "%{http_code}")
     if [ "$code" != "200" ]; then
       echo "Unable to download node: $code" && false
